756d961d0173f3fdf4c50d3c710b529ad291f321
kiyoshion <kiyoshion@gmail.com>
2021-07-08 20:47:45 +0900

    Update user

--- app/cruds/user.py
+++ app/cruds/user.py
@@ -3,6 +3,7 @@ from models import User
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_404_NOT_FOUND
 from uuid import UUID
+from schemas import User
 
 
 def read_users(db: Session):
@@ -18,3 +19,9 @@ def read_user(db: Session, user_id: UUID):
                             detail='Record not found.')
 
     return item
+
+def create_user(db: Session, user: User):
+    db_user = read_user(db, user_id=user.user_id)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return create_user(db=db, user=user)
